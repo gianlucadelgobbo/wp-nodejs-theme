7037ddfdce706fd3688ca1b16f17fb230ef8e9f8
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2016-10-17 19:47:49 +0200

    Steps

---
+++ app/helpers.js
@@ -0,0 +1,87 @@
+var WPAPI = require( 'wpapi' );
+
+exports.getEvent = function getEvent(req,callback) {
+  console.log(req.params.event);
+  var wp = new WPAPI({ endpoint: 'http://flyer.it/wp-json' });
+  wp.myCustomResource = wp.registerRoute( 'wp/v2', '/event/(?P<slug>)' );
+  wp.myCustomResource().slug(req.params.event).get(function( err, data ) {
+    console.log("//// Event");
+    console.log(data[0].capauthor[0]);
+    wp.myCustomResource = wp.registerRoute('wp/v2', '/capauthor/(?P<id>)');
+    var capauthors = [];
+    for (var a = 0; a < data[0].capauthor.length; a++) {
+      wp.myCustomResource().id(data[0].capauthor[a]).get(function( err2, data2 ) {
+        console.log("//// capauthor");
+        //console.log(err2 || data2);
+        console.log(data2.name);
+        wp.myCustomResource = wp.registerRoute( 'wp/v2', '/author/(?P<author>)' );
+        wp.myCustomResource().author(data2.name).get(function( err3, data3 ) {
+          console.log("//// author");
+          //console.log(err2 || data2);
+          console.log(data3.data.display_name);
+          capauthors.push(data3.data);
+          console.log(data[0].capauthor.length);
+          console.log(capauthors.length);
+          if (data[0].capauthor.length==capauthors.length){
+            data[0].authors = capauthors;
+            callback(data[0]);
+          }
+        });
+      });
+    }
+  });
+};
+exports.getEditionMenu = function getEvent(req,callback) {
+  var edition = req.params.edition ? req.params.edition : "2016-amsterdam";
+  console.log(edition);
+  var wp = new WPAPI({ endpoint: 'http://liveperformersmeeting.net/wp-json' });
+  wp.myCustomResource = wp.registerRoute( 'wp/v2', '/edition_parent/(?P<sluggg>)' );
+  wp.myCustomResource().sluggg(edition).get(function( err, data ) {
+    console.log(data);
+    wp.myCustomResource = wp.registerRoute( 'wp/v2', '/edition/(?P<edition>)' );
+    wp.myCustomResource().edition(edition).get(function( err, data2 ) {
+      console.log(data2);
+      data[data2.ID] = data2;
+      callback(data);
+    });
+  });
+};
+
+/*
+    wp.myCustomResource().id(data[0].capauthor[0]).get(function( err2, data2 ) {
+      console.log("//// capauthor");
+      console.log(err2 || data2);
+      console.log(data2.name);
+      wp.myCustomResource = wp.registerRoute( 'wp/v2', '/author/(?P<author>)' );
+      wp.myCustomResource().author(data2.name).get(function( err3, data3 ) {
+        console.log("//// author");
+        console.log(err3 || data3);
+        res.render('events', {
+          data: data
+        });
+      });
+    });
+    // do something with the returned posts
+
+  });
+    DB.accounts.findOne(q ,function(err, result) {
+      if (result) {
+        e.push({name:"vat_number",m:__("VAT number already in use")});
+        callback(e, o);
+      } else {
+        if (global._config.company.country == "Italy" && o.address.country == "Italy"){
+          //var q = (o.id ? {_id:{$ne: new ObjectID(o.id)},fiscal_code:o.fiscal_code} : {fiscal_code:o.fiscal_code});
+          DB.accounts.findOne({user:o.user}, function(err, result) {
+            if (result){
+              e.push({name:"fiscal_code",m:__("Fiscal code already in use")});
+            }
+            callback(e, o);
+          });
+        } else {
+          callback(e, o);
+        }
+      }
+    });
+  }
+};
+  */
--- app/routes/events.js
+++ app/routes/events.js
@@ -1,25 +1,10 @@
-var WPAPI = require( 'wpapi' );
-var wp = new WPAPI({ endpoint: 'http://flyer.it/wp-json' });
+var helpers = require('./../helpers');
 
 exports.get = function get(req, res) {
-	console.log(req.params.event);
-	wp.myCustomResource = wp.registerRoute( 'wp/v2', '/event/(?P<slug>)' );
-	wp.myCustomResource().slug(req.params.event).get(function( err, data ) {
-		console.log(data[0].author[0]);
-		wp.myCustomResource = wp.registerRoute( 'wp/v2', '/author/(?P<id>)' );
-		wp.myCustomResource().id(data[0].author[0]).get(function( err2, data2 ) {
-			console.log(err2 || data2);
-			console.log(data2.name);
-			wp.myCustomResource = wp.registerRoute( 'myplugin/v1', '/author/(?P<slug>)' );
-			wp.myCustomResource().slug(data2.name).get(function( err3, data3 ) {
-				console.log(err3 || data3);
-				res.render('events', {
-					data: data
-				});
-			});
+	helpers.getEditionMenu(req, function( submenu ) {
+		helpers.getEvent(req, function( result ) {
+			res.render('events', {data: result, submenu:submenu});
 		});
-		// do something with the returned posts
-
 	});
 };
 
