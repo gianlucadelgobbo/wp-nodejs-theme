637037ef77231913069faca4c95aeee7906167d2
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2017-01-10 15:42:05 +0100

    Design

---
+++ forever.json.js
@@ -0,0 +1,18 @@
+[
+  {
+    "uid": "app",
+    "append": true,
+    "watch": true,
+    "script": "app.js",
+    "sourceDir": "./",
+    "args": ["-site", "flyer"]
+  },
+  {
+    "uid": "app",
+    "append": true,
+    "watch": true,
+    "script": "app.js",
+    "sourceDir": "./",
+    "args": ["-site", "lpm"]
+  }
+]
