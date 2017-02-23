08624cc7bd8733a24ab6c93fa97e3f90312e2fe4
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2017-02-23 10:53:36 +0100

    Compression

--- sass/gulpfile.js
+++ sass/gulpfile.js
@@ -134,19 +134,33 @@ gulp.task('concat_common_scripts', function() {
 });
 gulp.task('concat_avnode_css', function() {
   return gulp.src([
-    config.publicDir + '/_common/css/fontello.min.css',
-    config.publicDir + '/_common/css/fontello-animation.min.css',
-    config.publicDir + '/_common/css/socialGalleryPluginLite.min.css',
-    config.publicDir + '/_common/css/cookielawinfo.min.css',
+    config.publicDir + '/_common/css/fontello.css',
+    config.publicDir + '/_common/css/fontello-animation.css',
+    config.publicDir + '/_common/css/socialGalleryPluginLite.css',
+    config.publicDir + '/_common/css/cookielawinfo.css',
     config.publicDir + '/avnode/css/bootstrap.min.css',
-    config.publicDir + '/_common/css/bootstrapXL.min.css',
+    config.publicDir + '/_common/css/bootstrapXL.css',
     config.publicDir + '/_common/css/style.css',
     config.publicDir + '/avnode/css/style.css',
   ])
-      .pipe(concat('combo.min.css'), {newLine: '\r\n'})
-      .pipe(gulp.dest(config.publicDir + '/avnode/css/'));
+    .pipe(concat('combo.min.css'), {newLine: '\r\n'})
+    .pipe(gulp.dest(config.publicDir + '/avnode/css/'));
 });
-gulp.task('default', [
+gulp.task('concat_flyer_css', function() {
+  return gulp.src([
+    config.publicDir + '/_common/css/fontello.css',
+    config.publicDir + '/_common/css/fontello-animation.css',
+    config.publicDir + '/_common/css/socialGalleryPluginLite.css',
+    config.publicDir + '/_common/css/cookielawinfo.css',
+    config.publicDir + '/flyer/css/bootstrap.min.css',
+    config.publicDir + '/_common/css/bootstrapXL.css',
+    config.publicDir + '/_common/css/style.css',
+    config.publicDir + '/flyer/css/style.css',
+  ])
+    .pipe(concat('combo.min.css'), {newLine: '\r\n'})
+    .pipe(gulp.dest(config.publicDir + '/flyer/css/'));
+});
+var tasklist = [
   'css_lpm_bs',
   'css_flyer_bs',
   'css_shockart_bs',
@@ -159,6 +173,78 @@ gulp.task('default', [
   'compress_js_lpm',
   'compress_js_flyer',
   'compress_js_shockart',
-  'concat_common_scripts',
-  'concat_avnode_css'
-]);
+  //'concat_common_scripts',
+  'concat_avnode_css',
+  'concat_flyer_css',
+  'concat_lpm_css',
+  'concat_shockart_css'
+];
+var editions = [
+  '2004-rome',
+  '2005-rome',
+  '2006-rome',
+  '2007-rome',
+  '2008-mex',
+  '2008-rome',
+  '2009-rome',
+  '2010-rome',
+  '2011-minsk',
+  '2011-rome',
+  '2012-rome',
+  '2013-cape-town',
+  '2013-mex',
+  '2013-rome',
+  '2014-eindhoven',
+  '2015-rome',
+  '2016-amsterdam',
+  '2017-amsterdam'
+];
+var excludeXL = [
+  '2011-minsk',
+  '2011-rome',
+  '2010-rome',
+  '2009-rome',
+  '2008-rome',
+  '2008-mex',
+  '2007-rome',
+  '2006-rome',
+  '2005-rome',
+  '2004-rome'
+];
+gulp.task('concat_lpm_css', function() {
+  for (var item in editions) {
+    var csslist = [
+      config.publicDir + '/_common/css/fontello.css',
+      config.publicDir + '/_common/css/fontello-animation.css',
+      config.publicDir + '/_common/css/socialGalleryPluginLite.css',
+      config.publicDir + '/_common/css/cookielawinfo.css',
+      config.publicDir + '/lpm/css/bootstrap.min.lpm.' + editions[item] + '.css',
+      config.publicDir + '/_common/css/style.lpm.css',
+      config.publicDir + '/lpm/css/style.lpm.' + editions[item] + '.css',
+    ];
+    if (excludeXL.indexOf(editions[item]) == -1) csslist.push(config.publicDir + '/_common/css/bootstrapXL.css');
+    bella(csslist,editions,item);
+
+  }
+});
+function bella (csslist,editions,item){
+  return gulp.src(csslist)
+    .pipe(concat('combo.lpm.' + editions[item] + '.min.css'), {newLine: '\r\n'})
+    .pipe(gulp.dest(config.publicDir + '/lpm/css/'));
+}
+gulp.task('concat_shockart_css', function() {
+  return gulp.src([
+    config.publicDir + '/_common/css/fontello.css',
+    config.publicDir + '/_common/css/fontello-animation.css',
+    config.publicDir + '/_common/css/socialGalleryPluginLite.css',
+    config.publicDir + '/_common/css/cookielawinfo.css',
+    config.publicDir + '/shockart/css/bootstrap.min.css',
+    config.publicDir + '/_common/css/bootstrapXL.css',
+    config.publicDir + '/_common/css/style.css',
+    config.publicDir + '/shockart/css/style.css',
+  ])
+    .pipe(concat('combo.min.css'), {newLine: '\r\n'})
+    .pipe(gulp.dest(config.publicDir + '/shockart/css/'));
+});
+
+gulp.task('default', tasklist);
