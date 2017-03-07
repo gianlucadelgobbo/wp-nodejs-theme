b95759cf82dde78de280b82e3ce843fc7b21a03d
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2017-03-07 12:22:05 +0100

    Compression

--- public/avnode/js/map.js
+++ public/avnode/js/map.js
@@ -59,7 +59,7 @@ function initialize() {
 
   var latlngbounds = new google.maps.LatLngBounds();
   for (i = 0; i < markers.length; i++) {
-    if (markers[i].type == "production" || markers[i].type == "customer") {
+    if (markers[i].type == "member") {
       var data = markers[i];
       var myLatlng = new google.maps.LatLng(data.lat, data.lng);
       latlngbounds.extend(myLatlng);
