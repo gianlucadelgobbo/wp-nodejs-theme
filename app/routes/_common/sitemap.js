41b9ccca97a4c8663675dccf7142274f76f49e90
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2017-06-12 10:10:46 +0200

    All sites

--- app/routes/_common/sitemap.js
+++ app/routes/_common/sitemap.js
@@ -4,65 +4,67 @@ var fnz = require('../../functions');
 var sez = config.sez["awards-and-grants"];
 
 exports.get = function get(req, res) {
-  res.header('Content-Type', 'text/xml');
-  var now = new Date();
-  now.setHours(now.getHours() - 4);
-  var isodate = fnz.ISODateString(now);
+  helpers.setSessions(req, function() {
+    res.header('Content-Type', 'text/xml');
+    var now = new Date();
+    now.setHours(now.getHours() - 4);
+    var isodate = fnz.ISODateString(now);
 
-  if (req.url == "/sitemap.xml") {
-    //console.log(req.url);
-    res.render(config.prefix+'/sitemap', {isodate:isodate});
+    if (req.url == "/sitemap.xml") {
+      //console.log(req.url);
+      res.render(config.prefix+'/sitemap', {isodate:isodate});
 
-  } else if (req.url == "/sitemap-home.xml") {
-    //console.log(req.url);
-    res.render('_common/sitemap-home', {isodate:isodate});
+    } else if (req.url == "/sitemap-home.xml") {
+      //console.log(req.url);
+      res.render('_common/sitemap-home', {isodate:isodate});
 
-  } else if (req.url == "/sitemap-pages.xml") {
-    //console.log(req.url);
-    res.render('_common/sitemap-pages', {isodate:isodate});
+    } else if (req.url == "/sitemap-pages.xml") {
+      //console.log(req.url);
+      res.render('_common/sitemap-pages', {isodate:isodate});
 
-  } else if (req.params.taxonomy) {
-    //console.log(req.params.taxonomy);
-    helpers.getPostType(req, req.params.taxonomy, function( results ) {
-      res.render('_common/sitemap-taxonomy', {results:results, isodate:isodate});
-    });
+    } else if (req.params.taxonomy) {
+      //console.log(req.params.taxonomy);
+      helpers.getPostType(req, req.params.taxonomy, function( results ) {
+        res.render('_common/sitemap-taxonomy', {results:results, isodate:isodate});
+      });
 
-  } else if (req.params.edition) {
-      var result = config.meta.editions[req.params.edition];
-      var now = new Date();
-      var date = new Date(result["wpcf-startdate"]*1000);
-      var timeDiff = (date.getTime() - now.getTime());
-      var diffDays = (timeDiff / (1000 * 3600 * 24));
-      var lastmod = diffDays < 0 ? fnz.ISODateString(date) : fnz.ISODateString(now);
-      var changefreq = diffDays < 0 ? "yearly" : "daily";
-      res.render('_common/sitemap-edition', {result:result, lastmod:lastmod, changefreq:changefreq});
-  } else if (req.params.exhibition) {
-    //console.log(req.params.exhibition);
-      var result = config.meta.editions[req.params.exhibition];
-      res.render('_common/sitemap-exhibition', {result:result, isodate:isodate});
-    //});
+    } else if (req.params.edition) {
+        var result = config.meta.editions[req.params.edition];
+        var now = new Date();
+        var date = new Date(result["wpcf-startdate"]*1000);
+        var timeDiff = (date.getTime() - now.getTime());
+        var diffDays = (timeDiff / (1000 * 3600 * 24));
+        var lastmod = diffDays < 0 ? fnz.ISODateString(date) : fnz.ISODateString(now);
+        var changefreq = diffDays < 0 ? "yearly" : "daily";
+        res.render('_common/sitemap-edition', {result:result, lastmod:lastmod, changefreq:changefreq});
+    } else if (req.params.exhibition) {
+      //console.log(req.params.exhibition);
+        var result = config.meta.editions[req.params.exhibition];
+        res.render('_common/sitemap-exhibition', {result:result, isodate:isodate});
+      //});
 
-  } else if (req.params.posttype) {
-    //console.log("eccomi");
-    //console.log(config.sez[req.params.posttype]);
-    helpers.getAll(req, config.sez[req.params.posttype], -1, 1, function( results ) {
-      if (req.params.posttype == "exhibitions") {
-        res.render('_common/sitemap-exhibitions', {results:results, isodate:isodate});
-      } else if (req.params.posttype == "editions") {
-        res.render('_common/sitemap-editions', {results:results, isodate:isodate});
-      } else {
-        res.render('_common/sitemap-posttype', {results:results, isodate:isodate});
-      }
-    });
-
-  } else if (req.params.users) {
-    //console.log(req.params.users);
-    helpers.getAllUsers(req, req.params.users, function( results ) {
+    } else if (req.params.posttype) {
       //console.log("eccomi");
-      //console.log("eccomi");
-      res.render('_common/sitemap-users', {results: results, baseurl:config.sitemap.users[req.params.users].baseurl});
-    });
-  }
+      //console.log(config.sez[req.params.posttype]);
+      helpers.getAll(req, config.sez[req.params.posttype], -1, 1, function( results ) {
+        if (req.params.posttype == "exhibitions") {
+          res.render('_common/sitemap-exhibitions', {results:results, isodate:isodate});
+        } else if (req.params.posttype == "editions") {
+          res.render('_common/sitemap-editions', {results:results, isodate:isodate});
+        } else {
+          res.render('_common/sitemap-posttype', {results:results, isodate:isodate});
+        }
+      });
+
+    } else if (req.params.users) {
+      //console.log(req.params.users);
+      helpers.getAllUsers(req, req.params.users, function( results ) {
+        //console.log("eccomi");
+        //console.log("eccomi");
+        res.render('_common/sitemap-users', {results: results, baseurl:config.sitemap.users[req.params.users].baseurl});
+      });
+    }
+  });
 };
 
 exports.getAll = function getAll(req, res) {
