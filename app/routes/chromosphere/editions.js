f25b74853a285dcfcfec7efea02464bf31518fb6
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2018-04-23 17:48:35 +0200

    GENERATOR FIX

--- app/routes/chromosphere/editions.js
+++ app/routes/chromosphere/editions.js
@@ -58,44 +58,45 @@ exports.getAll = function getAll(req, res) {
   });
 };
 exports.getMeta = function getMeta(req, res) {
-  var WPAPI = require( 'wpapi' );
-  //console.log("getMeta");
-  meta = {};
-  conta = [];
-  if (!req.query.generate){
-    res.render(config.prefix+"/meta_test", {meta:config.meta.editions});
-  } else {
-    getMetaSingle(config.editions[conta.length],req);
-    function getMetaSingle(val,req) {
-      //console.log("getMetaSingle 1 "+val);
-      //console.log('wp/v2/meta_data/editions/'+config.prefix+'/'+val);
-      var wp = new WPAPI({ endpoint: config.data_domain+(req.session.sessions.current_lang!=config.default_lang ? '/'+req.session.sessions.current_lang : '')+'/wp-json' });
-      wp.myCustomResource = wp.registerRoute( 'wp/v2', '/meta_data/(?P<sez>)/(?P<edition>)' );
-      wp.myCustomResource().edition(config.prefix+'/'+val).sez("editions").get(function( err, data ) {
-        //console.log("getMetaSingle 2");
-        //console.log(data);
-        meta[val] = data.meta.edition;
-        conta.push(val);
+  helpers.setSessions(req, function() {
+    var WPAPI = require( 'wpapi' );
+    //console.log("getMeta");
+    meta = {};
+    conta = [];
+    if (!req.query.generate){
+      res.render(config.prefix+"/meta_test", {meta:config.meta.editions});
+    } else {
+      getMetaSingle(config.editions[conta.length],req);
+      function getMetaSingle(val,req) {
+        //console.log("getMetaSingle 1 "+val);
         //console.log('wp/v2/meta_data/editions/'+config.prefix+'/'+val);
-        //console.log(conta.length +" - "+editions.length);
-        //console.log(req.query.check);
-        if (conta.length==config.editions.length) {
-          if (req.query.check){
-            res.render(config.prefix+"/meta_test", {meta:meta});
+        var wp = new WPAPI({ endpoint: config.data_domain+(req.session.sessions.current_lang!=config.default_lang ? '/'+req.session.sessions.current_lang : '')+'/wp-json' });
+        wp.myCustomResource = wp.registerRoute( 'wp/v2', '/meta_data/(?P<sez>)/(?P<edition>)' );
+        wp.myCustomResource().edition(config.prefix+'/'+val).sez("editions").get(function( err, data ) {
+          //console.log("getMetaSingle 2");
+          //console.log(data);
+          meta[val] = data.meta.edition;
+          conta.push(val);
+          //console.log('wp/v2/meta_data/editions/'+config.prefix+'/'+val);
+          //console.log(conta.length +" - "+editions.length);
+          //console.log(req.query.check);
+          if (conta.length==config.editions.length) {
+            if (req.query.check){
+              res.render(config.prefix+"/meta_test", {meta:meta});
+            } else {
+              require('jsonfile').writeFile(config.root+'/config/editions_'+config.prefix+'.json', meta, function(err) {
+                config.meta.editions = meta;
+                res.render(config.prefix+"/meta_test", {meta:config.meta.editions});
+              });
+            }
           } else {
-            require('jsonfile').writeFile(config.root+'/config/editions_'+config.prefix+'.json', meta, function(err) {
-              config.meta.editions = meta;
-              res.render(config.prefix+"/meta_test", {meta:config.meta.editions});
-            });
+            //console.log("getMetaSingle 3 "+editions[conta.length]);
+            getMetaSingle(config.editions[conta.length],req);
           }
-        } else {
-          //console.log("getMetaSingle 3 "+editions[conta.length]);
-          getMetaSingle(config.editions[conta.length],req);
-        }
-      });
+        });
+      }
     }
-  }
-
+  });
 };
 //select * from flyer_wp_20_terms,flyer_wp_20_term_relationships,flyer_wp_20_term_taxonomy where flyer_wp_20_term_taxonomy.term_taxonomy_id=flyer_wp_20_term_relationships.term_taxonomy_id and flyer_wp_20_term_taxonomy.term_id=flyer_wp_20_terms.term_id and  flyer_wp_20_term_relationships.object_id =49197;
 //wp.taxonomies().taxonomy( 'author' ).terms().get(function( err2, data2 ) {
--- app/routes/lpm/devs.js
+++ app/routes/lpm/devs.js
@@ -58,44 +58,45 @@ exports.getAll = function getAll(req, res) {
   });
 };
 exports.getMeta = function getMeta(req, res) {
-  var WPAPI = require( 'wpapi' );
-  //console.log("getMeta");
-  meta = {};
-  conta = [];
-  if (!req.query.generate){
-    res.render(config.prefix+"/meta_test", {meta:config.meta.devs});
-  } else {
-    getMetaSingle(config.devs[conta.length],req);
-    function getMetaSingle(val,req) {
-      //console.log("getMetaSingle 1 "+val);
-    //console.log('wp/v2/meta_data/devs/'+config.prefix+'/'+val);
-      var wp = new WPAPI({ endpoint: config.data_domain+(req.session.sessions.current_lang!=config.default_lang ? '/'+req.session.sessions.current_lang : '')+'/wp-json' });
-      wp.myCustomResource = wp.registerRoute( 'wp/v2', '/meta_data/(?P<sez>)/(?P<dev>)' );
-      wp.myCustomResource().dev(config.prefix+'/'+val).sez("devs").get(function( err, data ) {
-        //console.log("getMetaSingle 2");
-        //console.log(data);
-        meta[val] = data.meta.dev;
-        conta.push(val);
-        //console.log('wp/v2/meta_data/devs/'+config.prefix+'/'+val);
-        //console.log(conta.length +" - "+devs.length);
-        //console.log(req.query.check);
-        if (conta.length==config.devs.length) {
-          if (req.query.check){
-            res.render(config.prefix+"/meta_test", {meta:meta});
+  helpers.setSessions(req, function() {
+    var WPAPI = require( 'wpapi' );
+    //console.log("getMeta");
+    meta = {};
+    conta = [];
+    if (!req.query.generate){
+      res.render(config.prefix+"/meta_test", {meta:config.meta.devs});
+    } else {
+      getMetaSingle(config.devs[conta.length],req);
+      function getMetaSingle(val,req) {
+        //console.log("getMetaSingle 1 "+val);
+      //console.log('wp/v2/meta_data/devs/'+config.prefix+'/'+val);
+        var wp = new WPAPI({ endpoint: config.data_domain+(req.session.sessions.current_lang!=config.default_lang ? '/'+req.session.sessions.current_lang : '')+'/wp-json' });
+        wp.myCustomResource = wp.registerRoute( 'wp/v2', '/meta_data/(?P<sez>)/(?P<dev>)' );
+        wp.myCustomResource().dev(config.prefix+'/'+val).sez("devs").get(function( err, data ) {
+          //console.log("getMetaSingle 2");
+          //console.log(data);
+          meta[val] = data.meta.dev;
+          conta.push(val);
+          //console.log('wp/v2/meta_data/devs/'+config.prefix+'/'+val);
+          //console.log(conta.length +" - "+devs.length);
+          //console.log(req.query.check);
+          if (conta.length==config.devs.length) {
+            if (req.query.check){
+              res.render(config.prefix+"/meta_test", {meta:meta});
+            } else {
+              require('jsonfile').writeFile(config.root+'/config/devs_'+config.prefix+'.json', meta, function(err) {
+                config.meta.devs = meta;
+                res.render(config.prefix+"/meta_test", {meta:config.meta.devs});
+              });
+            }
           } else {
-            require('jsonfile').writeFile(config.root+'/config/devs_'+config.prefix+'.json', meta, function(err) {
-              config.meta.devs = meta;
-              res.render(config.prefix+"/meta_test", {meta:config.meta.devs});
-            });
+            //console.log("getMetaSingle 3 "+devs[conta.length]);
+            getMetaSingle(config.devs[conta.length],req);
           }
-        } else {
-          //console.log("getMetaSingle 3 "+devs[conta.length]);
-          getMetaSingle(config.devs[conta.length],req);
-        }
-      });
+        });
+      }
     }
-  }
-
+  });
 };
 //select * from flyer_wp_20_terms,flyer_wp_20_term_relationships,flyer_wp_20_term_taxonomy where flyer_wp_20_term_taxonomy.term_taxonomy_id=flyer_wp_20_term_relationships.term_taxonomy_id and flyer_wp_20_term_taxonomy.term_id=flyer_wp_20_terms.term_id and  flyer_wp_20_term_relationships.object_id =49197;
 //wp.taxonomies().taxonomy( 'author' ).terms().get(function( err2, data2 ) {
